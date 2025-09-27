bd347d58618e76ebc6cceb74b70880bd4d6830c6
bilalaitelhaj <bilal.aitelhaj@lycee-jeanrostand.fr>
2025-09-27 13:21:03 +0200

    creation des branch

--- 1erbranch.py
+++ 1erbranch.py
@@ -1,3 +1,9 @@
 print("maintenant quand on a pousser notre premier commit")
 print("nous allons cree une branch et pousser un commit")
 print("lui")
+print("un probleme est survenue tout fichier dans la deuxieme branch se sot désenregistrer tt seul")
+raison: "?"
+solution:"refaire une copie du commit sur la branch 2" 
+print("On branch branch1 Your branch is up to date with 'origin/branch1'.nothing to commit, working tree clean")
+erreur:oublie_denregistrement
+solution:enregistrer_localement_chaque_modif
